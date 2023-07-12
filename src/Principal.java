a19ffa629eade423f5891c1f9e14af035585418b
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-07-12 15:28:06 -0300

    feat: class `Principal` created

---
+++ src/Principal.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Principal {
+	public static void main(String[] args) {
+		List<Funcionario> funcionarios = new ArrayList<>();
+	}
+}
